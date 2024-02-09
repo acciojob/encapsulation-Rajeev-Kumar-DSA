1cba8d20a975c0d88de040123cf9982f5c798a88
Rajeev-Kumar-DSA <rajeevkr2061998@gmail.com>
2024-02-10 03:37:39 +0530

    Encapsulation Code using Getter and Setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly rwOnly = new RWOnly();
+
+        // Without using getter and setter method
+        // when try to set a value to name directly
+        // rwOnly.name = "Rajeev Kumar";  // Error: The field RWOnly.name is not visible
+
+        // when try to print the value of name directly
+        // System.out.println(rwOnly.name); // Error: The field RWOnly.name is not visible
+
+
+        // Set value to name using setter method
+        rwOnly.setName("Rajeev Kumar");
+
+
+        // Print value of name using getter method
+        System.out.println(rwOnly.getName());
+
+    }
 }
